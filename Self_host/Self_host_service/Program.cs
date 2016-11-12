98448ec855451d7de89007fa1570590f3331d89e
Alex_Fallen_Angel <AlexMrHam@ya.ru>
2016-11-12 12:20:13 +0300

    new

--- Self_host/Self_host_service/Program.cs
+++ Self_host/Self_host_service/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace Self_host_service
 {
@@ -10,6 +12,19 @@ namespace Self_host_service
     {
         static void Main(string[] args)
         {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+
         }
     }
 }
