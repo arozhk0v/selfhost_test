1706beac1625a87081c5eec8483562ffdd2f1782
Alex_Fallen_Angel <AlexMrHam@ya.ru>
2016-11-12 12:13:00 +0300

    two

--- Self_host/Self_host_service/Models/Row.cs
+++ Self_host/Self_host_service/Models/Row.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Self_host_service.Models
 {
+    /// <summary>
+    /// Класс строки.
+    /// </summary>
     class Row
     {
         public List<Exchangerate> Exchangerate { get; set; }
