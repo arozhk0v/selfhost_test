e0021b1aa5c668508e3557a52f749ae3ba370b61
fallenAngelBad <AlexMrHam@ya.ru>
2016-11-23 18:21:29 +0300

    Add files via upload

---
+++ Self_host/Self_host_service/DB/RequestAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_host_service.DB
+{
+    public class RequestAPI
+    {
+        /// <summary>
+        /// Метод для получения api.
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns></returns>
+        public static string GET(string Url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+    }
+}
