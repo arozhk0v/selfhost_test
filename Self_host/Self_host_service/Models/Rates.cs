15010e95df3a6a300eb2b15603e027e289f6a605
fallenAngelBad <AlexMrHam@ya.ru>
2016-11-23 18:21:05 +0300

    Add files via upload

---
+++ Self_host/Self_host_service/Models/Rates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Self_host_service.Models
+{
+    public class Rates
+    {
+        public double CAD { get; set; }
+        public double GBP { get; set; }
+        public double USD { get; set; }
+    }
+}
