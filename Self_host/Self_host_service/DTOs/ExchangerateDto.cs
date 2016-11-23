d1aa953b30a09a1a023dcdd2dd1e819ed2b0c24c
fallenAngelBad <AlexMrHam@ya.ru>
2016-11-23 18:20:44 +0300

    Add files via upload

---
+++ Self_host/Self_host_service/DTOs/ExchangerateDto.cs
@@ -0,0 +1,18 @@
+﻿using Self_host_service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_host_service.DTOs
+{
+    public class ExchangerateDto
+    {
+        public string @Base { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Rates rates { get; set; }
+    }
+}
